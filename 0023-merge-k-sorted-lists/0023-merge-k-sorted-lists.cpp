e86c19ab0131362d4739e22727316540a0764184
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-01-16 12:25:08 +0530

    Time: 93 ms (17.79%), Space: 18 MB (96.67%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,58 +11,58 @@
 class Solution {
 public:
         ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode * head;
-        if (!list1 && !list2){
-            return list1;
-        }
-        // if (list1 || list2){
-        //     return list1 || list2;
-        // }
-        if (!list1 && list2){
-            return list2;
-        }
-        if (list1 && !list2){
-            return list1;
-        }
-        if (list1->val < list2->val){
-            head = list1;
-            list1 = list1->next;
-        }
-        else{
-            head = list2;
-            list2 = list2->next;
-        }
-        ListNode * temp = head;
-        while (list1 && list2){
+            ListNode * head;
+            if (!list1 && !list2){
+                return list1;
+            }
+            // if (list1 || list2){
+            //     return list1 || list2;
+            // }
+            if (!list1 && list2){
+                return list2;
+            }
+            if (list1 && !list2){
+                return list1;
+            }
             if (list1->val < list2->val){
-                head->next = list1;
+                head = list1;
                 list1 = list1->next;
-                head = head->next;
             }
             else{
-                head->next = list2;
+                head = list2;
                 list2 = list2->next;
-                head = head->next;
             }
-        }
-        if(!list1 && list2){
-            head->next = list2;
-        }
-        if ( !list2 &&  list1){
-            head->next = list1;
-        }
-        return temp;
+            ListNode * temp = head;
+            while (list1 && list2){
+                if (list1->val < list2->val){
+                    head->next = list1;
+                    list1 = list1->next;
+                    head = head->next;
+                }
+                else{
+                    head->next = list2;
+                    list2 = list2->next;
+                    head = head->next;
+                }
+            }
+            if(!list1 && list2){
+                head->next = list2;
+            }
+            if ( !list2 &&  list1){
+                head->next = list1;
+            }
+            return temp;
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.size() == 0){
             return nullptr;
         }
- 
         ListNode* dummy1 = nullptr;
-
         for (int i = 0 ; i<lists.size();i++){
             dummy1 = mergeTwoLists(dummy1,lists[i]);
         }
         return dummy1;
     }
 };
+
+
