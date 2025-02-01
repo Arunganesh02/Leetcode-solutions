8d7083041540bd40a9fbccd212a7586570f63843
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-02-01 10:48:36 +0530

    Time: 3 ms (83.41%), Space: 17.9 MB (20.24%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,53 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        sub = [[],[],[],[],[],[],[],[],[]]
+        for i in range(9):
+            for j in range(9):
+                
+                if board[i][j]!=".":
+                    num = int(board[i][j])
+                    if i<3:
+                        if j<3:
+                            sub[0].append(num)
+                        elif j<6:
+                            sub[1].append(num)
+                        elif j<9:
+                            sub[2].append(num)
+                    elif i<6:
+                        if j<3:
+                            sub[3].append(num)
+                        elif j<6:
+                            sub[4].append(num)
+                        elif j<9:
+                            sub[5].append(num)
+                    elif i<9:
+                        if j<3:
+                            sub[6].append(num)
+                        elif j<6:
+                            sub[7].append(num)
+                        elif j<9:
+                            sub[8].append(num)                        
+        for i in range(9):
+            se = set()
+            for j in range(9):
+                if board[i][j]!= ".":
+                    if board[i][j] in se:
+                        return False
+                    se.add(board[i][j])
+
+        m = len(board[0])
+        for i in range(9):
+            se = set()
+            for j in range(9):
+                if board[j][i] != ".":
+                    if board[j][i] in se:
+                        return False
+                    se.add(board[j][i])
+
+        for i in sub:
+            se = set()
+            for j in i:
+                if j in se:
+                    return False
+                se.add(j)
+        return True
