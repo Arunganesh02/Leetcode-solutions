06ce95e561bd3754b129f54631ea4ef84e4cde5e
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-02-02 11:28:12 +0530

    Time: 0 ms (100%), Space: 17.7 MB (47.79%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -4,11 +4,21 @@ class Solution:
         r = 1
         c = 0
         while r<len(nums):
-            if nums[l] > nums[r]:
+            if nums[l]>nums[r]:
                 c += 1
             l += 1
             r += 1
         if nums[-1] > nums[0]:
-            c += 1
+            c+= 1
         return c<=1
+        # def issorted(li):
+        #     for i in range(1,len(li)):
+        #         if li[i-1] > li[i]:
+        #             return False
+        #     return True
         
+        # for i in range(len(nums)):
+        #     p = nums.pop()
+        #     nums.insert(0,p)
+        #     if issorted(nums): return True
+        # return False
