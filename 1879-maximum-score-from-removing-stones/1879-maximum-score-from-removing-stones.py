8173ecfa44d0b04b4f9f255519b741601634a103
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-02-02 19:27:53 +0530

    Time: 0 ms (100%), Space: 17.6 MB (75.12%) - LeetHub

--- 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.py
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.py
@@ -1,26 +1,8 @@
 class Solution:
     def maximumScore(self, a: int, b: int, c: int) -> int:
-        score = 0 
-        ma = max(a,b,c)
-        if ma == a :
-            if b+c == a:
-                return a
-            elif b+c < a:
-                return b+c
-            else:
-                return math.floor((b+c+a)/2) 
-        elif ma == b:
-            if a+c == b:
-                return b
-            elif c+a < b:
-                return c+a
-            else:
-                return math.floor((a+c+b)/2) 
-        else:
-            if b+a == c:
-                return c
-            elif b+a < c:
-                return b+a
-            else:
-                return math.floor(((b+a)+c) /2) 
-            
+        ma = sorted([a,b,c])
+        if ma[-1] == sum(ma[:2]):
+            return ma[-1]
+        elif sum(ma[:2]) < ma[-1]:
+            return sum(ma[:2])
+        return math.floor(sum(ma)/2)
