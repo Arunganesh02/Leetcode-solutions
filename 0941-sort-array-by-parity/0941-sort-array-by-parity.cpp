529dc624eb17dbd183294b1a28387b172b8655b4
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-02-10 12:43:31 +0530

    Time: 0 ms (100%), Space: 20.6 MB (38.57%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -13,32 +13,22 @@ public:
         // }
         // return {ans};
 
-        int temp = 0 ;
-        int count = 0 ;
-        int n = nums.size();
-        int j  = n-1;
-        for ( int i = 0  ; i < j ;){
+
+        int j;
+        for ( int i = 0 , j = 1 ; j < nums.size() && i < nums.size() ;){
             int p1= nums[i];
             int p2 = nums[j];
             if (p1%2 != 0  && p2%2 == 0){
-                temp = nums[i];
+                int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                count ++ ;
-                i++;
-                j--;
             }
             else if (p1%2!= 0 && p2%2 != 0){
-                j --;
+                j ++;
+                continue;
             }
-            else if (p1%2== 0 && p2%2 == 0){
-                i++;
-            }
-            else{
-                j--;
-                i++;
-            }
-
+            j++;
+            i++;
         }
         return nums;
         }
