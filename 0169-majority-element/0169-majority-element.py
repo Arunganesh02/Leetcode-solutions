1eb2e7559f86d6c6eb6f733794356d126678204b
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-02-11 09:38:18 +0530

    Time: 6 ms (61.6%), Space: 19.3 MB (81.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,12 +2,12 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         maj1 = c1 = 0
         for i in nums:
-            if maj1 == i:
-                c1 += 1
-            elif c1 == 0 :
+            if i == maj1:
+                c1+=1
+            elif c1 == 0:
                 maj1 = i
-                c1 += 1
-            else: 
+                c1 = 1
+            else:
                 c1 -= 1
         return maj1
         # nums.sort()
