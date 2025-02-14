80ba05d3825287eefe1ef06fb09c16a50634b756
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-02-15 00:48:04 +0530

    Time: 133 ms (75.49%), Space: 60 MB (99.47%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.py
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.py
@@ -5,27 +5,39 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dum = head.next
-        su = 0
-        newhead = None
-        while( dum.next != None):
-            if dum.val != 0:
-                su += dum.val
-            else:
-                if not newhead:
-                    newhead = ListNode(su)
-                    newdum = newhead
-                else:
-                    newhead.next = ListNode(su)
-                    newhead = newhead.next
-                su = 0
-            dum = dum.next
+        # dum = head.next
+        # su = 0
+        # newhead = None
+        # while( dum.next != None):
+        #     if dum.val != 0:
+        #         su += dum.val
+        #     else:
+        #         if not newhead:
+        #             newhead = ListNode(su)
+        #             newdum = newhead
+        #         else:
+        #             newhead.next = ListNode(su)
+        #             newhead = newhead.next
+        #         su = 0
+        #     dum = dum.next
         
-        if not newhead:
-            newhead = ListNode(su)
-            newdum = newhead
-        else:
-            newhead.next = ListNode(su)
-            newhead = newhead.next
+        # if not newhead:
+        #     newhead = ListNode(su)
+        #     newdum = newhead
+        # else:
+        #     newhead.next = ListNode(su)
+        #     newhead = newhead.next
 
-        return newdum
+        # return newdum
+        dum  = head
+        ptri = head
+        head = head.next
+        while (head.next):
+            if head.val != 0:
+                ptri.val += head.val
+            else:
+                ptri.next = head
+                ptri = head
+            head = head.next
+        ptri.next = None
+        return dum
