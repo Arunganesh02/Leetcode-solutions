41dd5afda1c1976bb5749b7dfc30ff27172022f7
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-02-25 11:55:22 +0530

    Time: 6 ms (8.69%), Space: 17.8 MB (97.41%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         ans = []
+        nums.sort()
         def backtrack ( li , start):
             nonlocal ans , nums
-            li.sort()
+
             if li not in ans:
                 ans.append(li)
             for i in range(start , len(nums)):
                 backtrack(li+[nums[i]] , i+1)
         backtrack([] , 0)
-
-
         return ans
