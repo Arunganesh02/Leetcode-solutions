51b6b1cf39e032d276fc07a9b7ac3719a93f87a2
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-02-27 12:19:11 +0530

    Time: 1953 ms (92.84%), Space: 18 MB (39.89%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -7,21 +7,14 @@ class Solution:
             if [istart , jstart] in seen:
                 return 
 
-            elif li == word:
-                lo = True
+            if li == word:
                 return True
 
-            elif li and li[-1] != word[poin-1]:
-                return
-
-            elif poin>=len(word):
-                return
-
-            elif jstart == len(board[0]) or jstart < 0 or istart == len(board) or istart < 0:
+            elif li and li[-1] != word[poin-1] or poin>=len(word) or jstart == len(board[0]) or jstart < 0 or istart == len(board) or istart < 0:
                 return
 
             return backtrack(li+board[istart][jstart] , poin+1 , istart , jstart +1 , seen + [[istart , jstart]])  or backtrack(li+board[istart][jstart] , poin+1 , istart+1 , jstart , seen + [[istart , jstart]]) or backtrack(li+board[istart][jstart] , poin+1 , istart , jstart-1 , seen + [[istart , jstart]]) or backtrack(li+board[istart][jstart] , poin+1 , istart-1 ,jstart , seen + [[istart , jstart]])
-            
+
         count = {}
         for c in word:
             count[c] = 1 + count.get(c, 0)
