5fdc22668bb443259ee7513a034510270f02eaaa
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-03-27 20:06:03 +0530

    Time: 7 ms (71.48%), Space: 21 MB (63.45%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,6 +1,6 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        li = []
-        for i in range(2**n):
-            li.append(i ^ (i>>1))
-        return li
+        return [i ^ (i>>1) for i in range(2**n)]
+        # for i in range(2**n):
+        #     li.append(i ^ (i>>1))
+        # return li
