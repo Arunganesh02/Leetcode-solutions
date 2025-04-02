83329e19d8938b09a3ac2ae2d04c8cbc581a565c
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-04-02 19:14:17 +0530

    Time: 42 ms (55.61%), Space: 17.7 MB (54.86%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -9,8 +9,10 @@ class Solution:
 
         for i in range(len(nums)):
             for j in range(i+1 , len(nums)):
+                he = nums[j+1:]
+                heapq._heapify_max(he)
                 if j != len(nums) -1:
-                    ans = max(ans , (nums[i] - nums[j]) * max(nums[j+1:]))
+                    ans = max(ans , (nums[i] - nums[j]) * he[0])
 
 
         return ans
