b5f8387f12e030d061f6d15174188001fa674b7b
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-04-26 01:21:58 +0530

    Time: 3 ms (22.26%), Space: 17.7 MB (65.48%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -11,4 +11,14 @@ class Solution:
             return left + up
         
         dp = [[-1 for i in range(n)] for j in range(m)]
-        return traverse(m-1 , n-1)
+        dp[0]= [1]*n
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(1,m):
+            for j in range(1,n):
+                left = dp[i][j-1]
+                up = dp[i-1][j]
+                dp[i][j] = left + up
+        # print(dp)
+        # return traverse(m-1 , n-1)
+        return dp[m-1][n-1]
