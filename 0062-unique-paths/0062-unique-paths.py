fcf81ff5c288747f27c59d55976c6c043a19973f
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-04-26 01:13:19 +0530

    Time: 4 ms (8.31%), Space: 17.9 MB (46.48%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -4,7 +4,6 @@ class Solution:
         def traverse(i , j):
             if i==0 and j == 0: return 1
             if i==-1 or j== -1 : return 0
-            # print(i , j)
             if dp[i][j] != -1 : return dp[i][j]
             left = traverse(i , j-1)
             up = traverse(i-1 , j)
@@ -12,6 +11,4 @@ class Solution:
             return left + up
         
         dp = [[-1 for i in range(n)] for j in range(m)]
-        # traverse(m-1 , n-1)
-        # print(dp)
         return traverse(m-1 , n-1)
