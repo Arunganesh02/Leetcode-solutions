d08ed9ac70071de58cbb213b423c2f5c07e8f04e
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-04-27 00:15:57 +0530

    Time: 11 ms (17.91%), Space: 19.8 MB (14.81%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,12 +1,8 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         def traverse(i , j ):
-            if i == 0 : 
-                # print(triangle[0][0])
-                dp[0][0] = triangle[0][0]
-                return triangle[0][0]
-            if j>=len(triangle): return 0
-            mini = float('inf')
+            if i == 0 : return triangle[0][0]
+            if j>=len(triangle): return float('inf')
             if dp[i][j] != 100000 : return dp[i][j]
             same, diff = 1000000 , 1000000
             if j < len(triangle[i-1]):
@@ -16,10 +12,10 @@ class Solution:
             mini = min(same , diff)
             dp[i][j] = mini
             return mini
+
         dp = [[100000 for i in range(len(triangle))] for j in range(len(triangle))]
-
+        minim = float('inf')
         for i in range(len(triangle[-1])):
-            traverse(len(triangle)-1 , i)
+            minim = min(minim ,traverse(len(triangle)-1 , i))
 
-
-        return min(dp[len(triangle)-1])
+        return minim
