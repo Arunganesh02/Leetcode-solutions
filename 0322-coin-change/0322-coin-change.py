3d2ecf1003e9e4f4d082ea48d6aa92032ed6f8a2
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-04-28 23:48:52 +0530

    Time: 1366 ms (8.95%), Space: 19.9 MB (27.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,24 +1,41 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        co = float('inf')
-        coins.reverse()
-        def traverse(indi , su):
-            # if su == 0 : return 1
-            if indi == 0 :
-                if su%coins[0] == 0 : return su//coins[0]
-                else: return float('inf')
-            if dp[indi][su] != -1 : return dp[indi][su]
-            nottake = float('inf')
-            nottake = 0 + traverse(indi-1 , su)
-            take = float('inf')
-            if coins[indi] <= su:
-                take = 1 + traverse(indi , su-coins[indi])
-            dp[indi][su] = min(nottake , take)
-            return min(nottake , take)
+        # def traverse(indi , su):
+
+        #     if indi == 0 :
+        #         if su%coins[0] == 0 : return su//coins[0]
+        #         else: return float('inf')
+        #     if dp[indi][su] != -1 : return dp[indi][su]
+        #     nottake = float('inf')
+        #     nottake = 0 + traverse(indi-1 , su)
+        #     take = float('inf')
+        #     if coins[indi] <= su:
+        #         take = 1 + traverse(indi , su-coins[indi])
+        #     dp[indi][su] = min(nottake , take)
+        #     return min(nottake , take)
+
         dp = [[-1 for i in range(amount+1)] for j in range(len(coins))]
-        ans = traverse(len(coins)-1 , amount)
-        if ans == float('inf') : return -1
-        else: return ans
+
+        for i in range(amount+1):
+            if i%coins[0] == 0 :
+                dp[0][i] = i//coins[0]
+            else:
+                dp[0][i] = float('inf')
+
+        for i in range(1,len(coins)):
+            for j in range(amount+1):
+                nottake = dp[i-1][j]
+                take = float('inf')
+                if coins[i] <= j:
+                    take = 1+dp[i][j-coins[i]]
+                dp[i][j] = min(nottake,take)
+                
+        # ans = traverse(len(coins)-1 , amount)
+        # print(dp)
+        if dp[len(coins)-1][amount] == float('inf') : return -1
+        return dp[len(coins)-1][amount]
+        # if ans == float('inf') : return -1
+        # else: return ans
         # return traverse(len(coins)-1 , amount)
         # if co == float('inf'): return -1
         # return co
