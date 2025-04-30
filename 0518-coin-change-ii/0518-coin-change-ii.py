32f2f8e628ba59ce6f920440af4920e2546f2228
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-04-30 10:41:48 +0530

    Time: 288 ms (49.52%), Space: 40.5 MB (45.89%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        def traverse(indi , su):
+            if su == 0 : return 1
+            if indi ==0 :
+                # if coins[indi] == 1 : return 1 
+                if su % coins[indi] == 0 : return 1
+                else: return 0
+            take = nottake = 0
+            if dp[indi][su] != -1 : return dp[indi][su]
+            nottake = traverse(indi-1 , su)
+            if coins[indi] <= su:
+                take = traverse(indi , su-coins[indi])
+            dp[indi][su] = take + nottake
+            return take + nottake
+
+    
+        dp = [[-1 for i in range(amount+1)] for j in range(len(coins))]
+        return traverse(len(coins)-1 , amount)
