f3489f71d875e3617c5d5cef06d83ffd6cc99eb0
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-05-03 11:38:10 +0530

    Time: 35 ms (1.17%), Space: 9.6 MB (8.97%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int traverse(int i , int j ,vector<vector<int>> &dp){
+        if (i==0 && j==0) return 1;
+        if (i<0 || j<0) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        int up = 0;
+        int left = 0;
+        up = traverse(i-1 , j , dp);
+        left = traverse(i , j-1 , dp);
+        dp[i][j] = up+left;
+        return up+left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1, -1));
+        dp[0][0] = 1;
+        long long up ,left ;
+        for (int i = 0 ; i<=m;i++){
+            // up = 0;
+            // left = 0;
+            for (int j = 0 ; j<=n;j++){
+                up = 0;
+                left = 0;
+                cout<<i <<" "<<j <<"\n";
+                if (i-1 >= 0 ){
+                    up = dp[i-1][j];
+                }
+                if (j-1 >= 0){
+                    left = dp[i][j-1];
+                    // cout<<dp[i][j-1]<<'\n';
+                }
+                // cout<<up << " "<<left<<"\n";
+                if (i==0 && j== 0);
+                else dp[i][j] = left + up;
+            }
+        }
+        return dp[m-1][n-1];
+
+    }
+};
