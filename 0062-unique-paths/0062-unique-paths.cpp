f3c182cd4d687c912d4d1de58d5ea0dbd294e0f0
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-05-03 11:38:51 +0530

    Time: 0 ms (100%), Space: 9.3 MB (32.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -16,20 +16,15 @@ public:
         dp[0][0] = 1;
         long long up ,left ;
         for (int i = 0 ; i<=m;i++){
-            // up = 0;
-            // left = 0;
             for (int j = 0 ; j<=n;j++){
                 up = 0;
                 left = 0;
-                cout<<i <<" "<<j <<"\n";
                 if (i-1 >= 0 ){
                     up = dp[i-1][j];
                 }
                 if (j-1 >= 0){
                     left = dp[i][j-1];
-                    // cout<<dp[i][j-1]<<'\n';
                 }
-                // cout<<up << " "<<left<<"\n";
                 if (i==0 && j== 0);
                 else dp[i][j] = left + up;
             }
