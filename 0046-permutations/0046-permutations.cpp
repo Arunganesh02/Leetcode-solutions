a10d6aef80501c8e4178bdb98513b3de130f6995
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-05-04 19:01:27 +0530

    Time: 14 ms (5.13%), Space: 15.5 MB (5.47%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> ans ;
-    void back(vector<int> li , set<int> se ,vector<int> &nums){
-        if(li.size() == nums.size()){
+    vector<vector<int>> ans;
+    void traverse( vector<int> li , set<int> seen , vector<int> &nums){
+        if (li.size() == nums.size()){
             ans.push_back(li);
             return ;
         }
         for (int i = 0 ; i<nums.size() ; i++){
-            if (se.count(i) == 0){
+            if (seen.count(i) == 0){
+                seen.insert(i);
                 li.push_back(nums[i]);
-                se.insert(i);
-                back(li , se , nums);
+                traverse(li , seen , nums);
                 li.pop_back();
-                se.erase(i);
+                seen.erase(i);
             }
         }
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
-        set<int> se;
-        back({} , se , nums);
+        set<int> seen;
+        traverse({} ,seen , nums );
         return ans;
     }
 };
