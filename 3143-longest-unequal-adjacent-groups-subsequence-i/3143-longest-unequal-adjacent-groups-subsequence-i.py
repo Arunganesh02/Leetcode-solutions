147733cb2c474c54bcb3cae8d663aee3c57098a4
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-05-15 23:14:29 +0530

    Time: 3 ms (28.03%), Space: 18.4 MB (5.36%) - LeetHub

---
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        def traverse(indi , li):
+            if indi == 0 : 
+                li.append(words[indi])
+                return li
+            take = []
+            if dp[indi] != -1 : return dp[indi]
+            if groups[indi-1] != groups[indi]:
+                take = traverse(indi-1 , li + [words[indi]])
+            nottake = traverse(indi-1 , li)
+            if len(take)>len(nottake): 
+                dp[indi] = take
+                return take
+            else: 
+                dp[indi] = nottake
+                return nottake
+
+        dp = [-1 for i in range(len(words)+1)]
+        return list(reversed(traverse(len(words)-1 , [])))
