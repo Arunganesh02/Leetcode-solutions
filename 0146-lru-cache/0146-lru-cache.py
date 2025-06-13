72391cb8875f431a84b6cd96cab2988c1984d269
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-06-13 20:33:05 +0530

    Time: 107 ms (88.37%), Space: 78.5 MB (46.37%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,48 +1,66 @@
 class Node:
-    def __init__(self,key , value):
-        self.value = value
+    def __init__(self , value,key ):
+        self.val = value
         self.next = None
         self.prev = None
         self.key = key
 
-
 class LRUCache:
+
     def __init__(self, capacity: int):
+        self.cap = capacity
         self.d = {}
         self.head = Node(-1,-1)
         self.tail = Node(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.capacity = capacity
-    def deleteNode(self,node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
 
     def insertAfterHead(self,node):
-        node.next = self.head.next
-        self.head.next.prev = node
-        node.prev = self.head
-        self.head.next = node
 
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next  = node
+
+    def deleteBeforeTail(self):
+     
+        node = self.tail.prev
+        self.tail.prev = node.prev 
+        node.prev.next = self.tail
+        return node.key
+
+    def replaceNode(self,node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
     def get(self, key: int) -> int:
         if key in self.d:
-            self.deleteNode(self.d[key])
-            self.insertAfterHead(self.d[key])
-            return self.d[key].value
-        else:
-            return -1
-
-    def put(self, key: int, value: int) -> None:
-        if key not in self.d:
-            if len(self.d) == self.capacity:
-                del self.d[self.tail.prev.key]
-                self.deleteNode(self.tail.prev)
-        else:
-            self.deleteNode(self.d[key])
-        new = Node(key,value)
-        self.insertAfterHead(new)
-        self.d[key] = new
+            node = self.d[key]
+            self.replaceNode(node)
+            self.insertAfterHead(node)
+            return node.val
+        else: return -1
         
+    def put(self, key: int, value: int) -> None:
+        if key in self.d:
+            node = self.d[key]
+            self.replaceNode(node)
+            node.val = value
+            self.insertAfterHead(node)
+            return
+
+        if len(self.d) < self.cap:
+            node = Node(value , key)
+            self.insertAfterHead(node)
+            self.d[key] = node
+        else:
+            deletedKey = self.deleteBeforeTail()
+            node = Node(value,key)
+            self.insertAfterHead(node)
+            del self.d[deletedKey]
+            self.d[key] = node
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
