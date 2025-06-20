c2b24eabf1b9bac231b29fbc3c73e493da827ba0
Arunganesh02 <91817924+Arunganesh02@users.noreply.github.com>
2025-06-20 21:57:34 +0530

    Time: 2 ms (34.74%), Space: 17.7 MB (99.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,35 +1,19 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
-        def backtrack(li):
-            nonlocal n , ans
+        def backtrack(li , le , ri):
+
+            if le - ri < 0:
+                return
+                
             if len(li) == n*2:
-                st = []
-                lo = 1
-                c1 = 0
-                c2 = 0
-                for i in li:
-                    if i == "(":
-                        c1+=1
-                    else:
-                        c2+=1
-                    if i=="(":
-                        st.append(i)
-                    else:
-                        if len(st) != 0 and st[-1] != "(":
-                            lo = 0
-                            break
-                        else:
-                            if len(st) != 0:
-                                st.pop()
-                # print(st)
-                if len(st) == 0 and c1==c2==n:
+                if le-ri == 0:
                     ans.append(li)
                 return
             
-            for i in "()":
-                backtrack(li+i)
-            
-        backtrack("")
+            backtrack(li+"(" ,le+1 , ri)
+            backtrack(li+")" ,le ,ri +1)
+    
+        backtrack("" , 0 , 0)
 
         return ans
